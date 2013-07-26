60c38a90e3ff90259df901767bc04bf13015239e
Vlad Dolezal <me@vladdolezal.com>
2013-07-26 13:23:20 +0200

    Made ai.ai_beat_most_common keep track of opponent moves encountered (rather than re-generating the count every round), significantly improving performance when playing a large number of rounds.

--- ai/ai_beat_most_common.py
+++ ai/ai_beat_most_common.py
@@ -1,16 +1,18 @@
 """An AI that always throws whatever would defeat opponent's most common throw."""
 
+throw_occurences = {'r': 0,
+					'p': 0,
+					's': 0}
+beats = {'r': 'p',
+		 'p': 's',
+		 's': 'r'}
 
 def move(data):
 	moves = data['moves']
 	player_number = data['player_number']
-	throw_occurences = {}
+	if moves: # If this isn't the first round.
+		# Add opponent's last move to cumulative count.
+		throw_occurences[moves[-1][-player_number]] += 1
 
-	beats = {'r': 'p',
-			 'p': 's',
-			 's': 'r'}
-
-	for throw in ['r', 'p', 's']:
-		throw_occurences[throw] = len([move for move in moves if move[-player_number] == throw])
-	opps_most_common_throw = sorted(throw_occurences, key = lambda thing: throw_occurences[thing])[-1]
+	opps_most_common_throw = sorted(throw_occurences, key = lambda x: throw_occurences[x])[-1]
 	return beats[opps_most_common_throw]
