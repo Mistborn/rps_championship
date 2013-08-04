8cae5d97a86e99671ffe703d684bc4f7227468bb
Vlad Dolezal <me@vladdolezal.com>
2013-08-04 10:17:08 +0200

    Added a new AI, ai_adaptive2 that not only considers several factors when deciding what to throw, but also changes the weighing of those factors based on which ones were successful in past rounds. Performs WAY better than ai_adaptive against other algorithms we have here, and beats ai_adaptive itself too.

--- ai/ai_adaptive2.py
+++ ai/ai_adaptive2.py
@@ -5,36 +5,47 @@ from collections import defaultdict
 from ai_templates.ai import AI
 
 
-class AIAdaptive(AI):
+class AIAdaptive2(AI):
 	"""An AI that adapts its moves based on what it thinks the opponent is most
 	likely to throw next. Considers the following:
 	- total throws made by opponent
 	- total throws made by self
 	- for each 5-long streak of own throws, what the opponent did next
 	- for each 5-long streak of opponent throws, what the opponent did next
+
+	On top of that, this AI changes the weight of each of those factors based
+	on which ones have been successful so far.
 	"""
 
-	def __init__(self, name='ai_adaptive'):
+	def __init__(self, name='ai_adaptive2'):
 		self.__name__ = name
 
-	own_throws = {'r': 0,
-				  'p': 0,
-				  's': 0}
+		self.own_throws = {'r': 0,
+						  'p': 0,
+						  's': 0}
 
-	opp_throws = {'r': 0,
-				  'p': 0,
-				  's': 0}
+		self.opp_throws = {'r': 0,
+						  'p': 0,
+						  's': 0}
 
-	# Create a dict mapping each sequence of own 5 throws to what throw the
-	# opponent did next, how many times.
-	own_throws_mapping = defaultdict(lambda: defaultdict(int))
-	# Same for the opponent's throws.
-	opp_throws_mapping = defaultdict(lambda: defaultdict(int))
-	# Example:
-	# opp_throws_mapping[('r','r','p','s','p')] == {'r': 1}
-	#  means that at some point in the past, the opponent played the sequence
-	# of moves ('r', 'r', 'p', 's', 'p'), and then followed it up with 'r'. He
-	# played that particular sequence of moves only once so far.
+		# Initialize the weights for each of the move decision factors
+		self.weights = [1, 1, 1, 1]
+
+		# Create a dict mapping each sequence of own 5 throws to what throw the
+		# opponent did next, how many times.
+		self.own_throws_mapping = defaultdict(lambda: defaultdict(int))
+		# Same for the opponent's throws.
+		self.opp_throws_mapping = defaultdict(lambda: defaultdict(int))
+		# Example:
+		# opp_throws_mapping[('r','r','p','s','p')] == {'r': 1}
+		#  means that at some point in the past, the opponent played the sequence
+		# of moves ('r', 'r', 'p', 's', 'p'), and then followed it up with 'r'. He
+		# played that particular sequence of moves only once so far.
+
+		# Initialize keeping track of move guesses for the purposes of updating
+		# the weights of each move decision factor.
+		self.guesses = [[],[],[],[]]
+
 
 	def move(self, data):
 
@@ -54,10 +65,34 @@ class AIAdaptive(AI):
 
 		moves = data['moves']
 		player_number = data['player_number']
+
 		if moves:
 			# Add each player's moves to a running total.
 			self.own_throws[moves[-1][player_number-1]] += 1
 			self.opp_throws[moves[-1][-player_number]] += 1
+			
+			# Update factor weighing based on which ones predicted the
+			# opponent's move well.
+			opp_move = moves[-1][-player_number]
+			desired_move = self.beats[opp_move]
+			for index in range(4):
+				# If the factor abstained from influencing the decision.
+				if len(self.guesses[index]) in [0, 3]:
+					pass
+				# If the factor was wrong.
+				elif desired_move not in self.guesses[index]:
+					self.weights[index] *= 0.9
+				# If the factor guessed exactly the correct move
+				elif len(self.guesses[index]) == 1:
+					self.weights[index] *= 1.1
+				# If the factor suggested two possibilities, one of them right.
+				elif len(self.guesses[index]) == 2:
+					self.weights[index] *= 1.04
+				else:
+					assert False, "Sanity check. Shouldn't get here."
+				# Clean up the guess for next turn.
+				del self.guesses[index][:]
+
 		if len(moves) < 6:
 			# Play randomly for the first six rounds
 			return random.choice(['r','p','s'])
@@ -81,15 +116,18 @@ class AIAdaptive(AI):
 			most_common_throws = [throw for throw in self.opp_throws if
 								  self.opp_throws[throw] == max(self.opp_throws.values())]
 			for throw in most_common_throws:
-				choices[self.beats[throw]] += 1/len(most_common_throws)
+				choices[self.beats[throw]] += 1/len(most_common_throws)*self.weights[0]
+				# Update our guesses for weigthing the factors next turn.
+				self.guesses[0].append(self.beats[throw])
 
 			# 2. Our most common throw so far. Assume the opponent will try to
 			# beat it, and stay one step ahead of him.
 			most_common_throws = [throw for throw in self.own_throws if
 								  self.own_throws[throw] == max(self.own_throws.values())]
 			for throw in most_common_throws:
-				choices[self.beats[self.beats[throw]]] += 1/len(most_common_throws)
-			
+				choices[self.beats[self.beats[throw]]] += 1/len(most_common_throws)*self.weights[1]
+				self.guesses[1].append(self.beats[self.beats[throw]])
+
 			# 3. Check the last 5 moves made by opponent. If he made the same 5
 			# moves at some point in the past, see what he followed it up with,
 			# and try to beat that.
@@ -99,7 +137,8 @@ class AIAdaptive(AI):
 				most_common_throws = [throw for throw in self.opp_throws_mapping[last_5_opp_moves] if
 									  self.opp_throws_mapping[last_5_opp_moves][throw] == most_common_no]
 				for throw in most_common_throws:
-					choices[self.beats[throw]] += 1/len(most_common_throws)
+					choices[self.beats[throw]] += 1/len(most_common_throws)*self.weights[2]
+					self.guesses[2].append(self.beats[throw])
 
 			# 4. Check the last 5 moves made by ourselves. If we made the same 5
 			# moves at some point in the past, see what the opponent followed up
@@ -110,7 +149,9 @@ class AIAdaptive(AI):
 				most_common_throws = [throw for throw in self.own_throws_mapping[last_5_own_moves] if
 									  self.own_throws_mapping[last_5_own_moves][throw] == most_common_no]
 				for throw in most_common_throws:
-					choices[self.beats[throw]] += 1/len(most_common_throws)
+					choices[self.beats[throw]] += 1/len(most_common_throws)*self.weights[3]
+					self.guesses[3].append(self.beats[throw])
+
 
 			# And now let's choose!
 
